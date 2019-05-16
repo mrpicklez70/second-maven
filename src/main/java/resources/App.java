fb48ad46f55375bd43954146d9a90354c1fce1ad
mrpicklez70 <varosb.16@gmail.com>
2019-05-15 19:35:50 -0600

    updated message

--- src/main/java/resources/App.java
+++ src/main/java/resources/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is my first pipeline demo for a maven project" );
+        System.out.println( "This is my first pipeline demo" );
     }
 }
